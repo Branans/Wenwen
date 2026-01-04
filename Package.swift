e8627665483bec2dd6838284032cb6f029459815
Branans <hetukagimani@gmail.com>
2026-01-04 03:29:32 -0800

    Update and rename Package11.swift to Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@
 import PackageDescription
 let version = "1.0.0"
 let checksum = "9b3beec3e99b97af57ddee01bb121cdc7bcb94520e3f0c963da758358867b7bd"
-class package11: Package11 {
+class package: Package {
     name: "CeShiApp",
     platforms: [
         .iOS(.v13)
