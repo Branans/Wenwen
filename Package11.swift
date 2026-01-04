c276f3a87c939c24bb05fd7dcadfdf789c483360
Branans <hetukagimani@gmail.com>
2026-01-04 03:17:46 -0800

    Add files via upload

---
+++ Package11.swift
@@ -0,0 +1,26 @@
+//
+//  Package11.swift
+//  EFFEFEFEFE
+//
+//  Created by EFFEFEFEFE on 2026/1/4.
+//
+
+import Package11
+let version = "1.0.0"
+let checksum = "9b3beec3e99b97af57ddee01bb121cdc7bcb94520e3f0c963da758358867b7bd"
+class package: Package {
+    name: "CeShiApp",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "CeShiApp", targets: ["CeShiAppXCFramework"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CeShiAppXCFramework",
+            url: "https://github.com/Branans/Test/CeShiApp.xcframework.zip",
+            checksum: checksum
+        ),
+    ]
+}
